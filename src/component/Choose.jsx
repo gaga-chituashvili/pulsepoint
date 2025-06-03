ba800d30fc3ee3087413696c02275df49c8f598d
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-06-03 16:42:07 +0400

    add_choose

--- src/component/Choose.jsx
+++ src/component/Choose.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import secure1 from "./images/secure1.png";
+import mask from "./images/mask.png";
+import mask1 from "./images/mask1.png";
+import feature from "./images/Feature.png";
 
 const Choose = () => {
   return (
-    <div>Choose</div>
-  )
-}
+    <section className="flex flex-col items-center gap-y-[40px]">
+      <h3>Why choose us?</h3>
+      <section className="flex gap-x-[16px]">
+         <article className="flex flex-col gap-y-[16px] items-center px-[12px]">
+        <img src={secure1} alt="secure" />
+       <article className="flex flex-col items-center gap-y-[6px]"> <span>safe & secure gaming</span>
+        <span className="text-center whitespace-pre-line"> Praesent facilisis massa vitae sapien
+          {"\n"}rutrum, a posuere. Etiam sollicitudin 
+          {"\n"}neque ac malesuada.
+          </span></article>
+      </article>
+       <article className="flex flex-col gap-y-[16px] items-center px-[12px]">
+        <img src={mask} alt="mask" />
+        <article className="flex flex-col items-center gap-y-[6px]">  <span>variety of games</span>
+        <span className="text-center whitespace-pre-line"> Lorem ipsum dolor sit amet, consectetur 
+          {"\n"}adipiscing elit. Fusce ullamcorper
+          {"\n"}vulputate magna.
+          </span></article>
+      </article>
+       <article className="flex flex-col gap-y-[16px] items-center px-[12px]">
+        <img src={mask1} alt="mask1" />
+       <article className="flex flex-col items-center gap-y-[6px]">   <span>fast & better experience</span>
+        <span className="text-center whitespace-pre-line">Nullam porta at orci non molestie. 
+          {"\n"}Aenean vel diam erat. Nullam dignissim
+          {"\n"}lorem pulvinar dui finibus ultrices.
+          </span></article>
+      </article>
+        <article className="flex flex-col gap-y-[16px] items-center px-[12px]">
+        <img src={feature} alt="feature" />
+        <article className="flex flex-col items-center gap-y-[6px] ">  <span>improved look & feel</span>
+        <span className="text-center whitespace-pre-line">Lorem ipsum dolor sit amet, consectetur 
+          {"\n"}adipiscing elit. Fusce ullamcorper 
+          {"\n"}vulputate magna.
+          </span></article>
+      </article>
+      </section>
+    </section>
+  );
+};
 
-export default Choose
+export default Choose;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Cover from '../component/Cover'
 import Gaiming from '../component/Gaiming'
+import Choose from '../component/Choose'
 
 const Home = () => {
   return (
     <>
     <Cover/>
     <Gaiming/>
+    <Choose/>
     </>
   )
 }
